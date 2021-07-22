9f01b9b3253406d9c2706d05b8f5f10aa5be181a
Kareem Draz <kareemdraz@myyz-0792.investcloud.int>
2021-07-21 22:12:31 -0400

    Java updates

--- src/MyFirstJavaCode.java
+++ src/MyFirstJavaCode.java
@@ -1,7 +1,44 @@
+import groovy.console.ui.SystemOutputInterceptor;
 
+import java.awt.*;
+import java.util.Date;
 
 public class MyFirstJavaCode {
     public static void main (String[] args) {
+
+        byte age = 30, temp = 20;
+        Date now = new Date();
+        now.getTime();
+        System.out.println(now);
+
+        byte x = 1;
+
+
+        Point point1 = new Point();
+        point1.y = 1;
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
+
+
+        String message = "Hellooooo Java" + "!!!";
+        boolean doesItEndWith = message.endsWith("!!");
+        System.out.println(message.length());
+
+        final float pi = 3.14F;
+//        pi = 1;
+
+
+
+
+        age = 35;
+        long viewsCount = 123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
         System.out.println("Hello World");
+        System.out.println(age);
+        System.out.println(viewsCount);
+
     }
 }
