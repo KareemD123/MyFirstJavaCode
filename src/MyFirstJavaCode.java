1af8621202cd6e33f95ba67e9d82a7db83df6339
Kareem Draz <kareemdraz@myyz-0792.investcloud.int>
2021-07-22 23:10:10 -0400

    Created a new window

--- src/MyFirstJavaCode.java
+++ src/MyFirstJavaCode.java
@@ -2,48 +2,43 @@ import groovy.console.ui.SystemOutputInterceptor;
 
 import java.awt.*;
 import java.util.Date;
+import javax.swing.JFrame;
+
 
 public class MyFirstJavaCode {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-        byte age = 30, temp = 20;
-        Date now = new Date();
-        now.getTime();
-//        System.out.println(now);
-
-//        byte x = 1;
-
-
-        Point point1 = new Point();
-        point1.y = 1;
-        Point point2 = point1;
-        point1.x = 2;
-//        System.out.println(point2);
-
-
-        String message = "Hellooooo Java" + "!!!";
-        boolean doesItEndWith = message.endsWith("!!");
-//        System.out.println(message.length());
-
-        final float pi = 3.14F;
-//        pi = 1;
-
-    double result = 10 / 3;
-    int x = 1;
-    int y = x++;
-    System.out.println(y);
-
-
-
-
-        age = 35;
-        long viewsCount = 123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = false;
-//        System.out.println("Hello World");
-//        System.out.println(age);
-//        System.out.println(viewsCount);
-
+        JFrame myFrame = new JFrame("MY FIRST WINDOW");
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setVisible(true);
     }
+
+//        byte age = 30, temp = 20;
+//        Date now = new Date();
+//        now.getTime();
+////        System.out.println(now);
+////        byte x = 1;
+//        Point point1 = new Point();
+//        point1.y = 1;
+//        Point point2 = point1;
+//        point1.x = 2;
+////        System.out.println(point2);
+//        String message = "Hellooooo Java" + "!!!";
+//        boolean doesItEndWith = message.endsWith("!!");
+////        System.out.println(message.length());
+//        final float pi = 3.14F;
+////        pi = 1;
+//    double result = 10 / 3;
+//    int x = 1;
+//    int y = x++;
+//    System.out.println(y);
+//
+//        age = 35;
+//        long viewsCount = 123_456_789L;
+//        float price = 10.99F;
+//        char letter = 'A';
+//        boolean isEligible = false;
+////        System.out.println("Hello World");
+////        System.out.println(age);
+////        System.out.println(viewsCount);
 }
